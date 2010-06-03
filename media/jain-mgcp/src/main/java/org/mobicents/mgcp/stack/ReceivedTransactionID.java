5bc2a471a2b5b4c9fe19545b2bc23ea51f6f55b8
pavel.slegr <pavel.slegr@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2010-06-03 11:20:43 +0000

    git-svn-id: https://mobicents.googlecode.com/svn/branches/jbcp/5.x-mead@12055 bf0df8d0-2c1f-0410-b170-bd30377b63dc

---
+++ media/jain-mgcp/src/main/java/org/mobicents/mgcp/stack/ReceivedTransactionID.java
@@ -0,0 +1,41 @@
+package org.mobicents.mgcp.stack;
+
+import java.io.Serializable;
+import java.net.InetAddress;
+
+/**
+ * The ID of a received MGCP transaction.
+ * 
+ * @author eduardomartins
+ * 
+ */
+public class ReceivedTransactionID implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2376823218467778072L;
+
+	protected final int transactionHandle;
+	protected final InetAddress remoteAddress;
+	protected final int remotePort;
+
+	protected ReceivedTransactionID(int transactionHandle, InetAddress remoteAddress, int remotePort) {
+		this.transactionHandle = transactionHandle;
+		this.remoteAddress = remoteAddress;
+		this.remotePort = remotePort;
+	}
+
+	public boolean equals(Object obj) {
+		if (obj != null && obj.getClass() == this.getClass()) {
+			ReceivedTransactionID other = (ReceivedTransactionID) obj;
+			return this.transactionHandle == other.transactionHandle && this.remoteAddress.equals(other.remoteAddress)
+					&& this.remotePort == other.remotePort;
+		}
+		return false;
+	}
+
+	public int hashCode() {
+		return (transactionHandle * 31 + remoteAddress.hashCode()) * 31 + remotePort;
+	}
+}
