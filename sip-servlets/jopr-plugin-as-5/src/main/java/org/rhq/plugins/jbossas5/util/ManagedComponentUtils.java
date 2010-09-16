5249f0ef2c9e959798b10ecac0e71ebd22b80973
jean.deruelle <jean.deruelle@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2010-09-16 16:43:45 +0000

    Update Issue 1813
    Not yet working butheavily refactored
    
    git-svn-id: https://mobicents.googlecode.com/svn/branches/jbcp/5.x-mead@14121 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- sip-servlets/jopr-plugin-as-5/src/main/java/org/rhq/plugins/jbossas5/util/ManagedComponentUtils.java
+++ sip-servlets/jopr-plugin-as-5/src/main/java/org/rhq/plugins/jbossas5/util/ManagedComponentUtils.java
@@ -1,47 +1,58 @@
 /*
- * RHQ Management Platform
+ * Jopr Management Platform
  * Copyright (C) 2005-2009 Red Hat, Inc.
  * All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation version 2 of the License.
+ * it under the terms of the GNU General Public License, version 2, as
+ * published by the Free Software Foundation, and/or the GNU Lesser
+ * General Public License, version 2.1, also as published by the Free
+ * Software Foundation.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ * GNU General Public License and the GNU Lesser General Public License
+ * for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * and the GNU Lesser General Public License along with this program;
+ * if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 package org.rhq.plugins.jbossas5.util;
 
-import java.util.Set;
-import java.util.EnumSet;
 import java.io.Serializable;
+import java.util.EnumSet;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Comparator;
 
-import org.rhq.plugins.jbossas5.factory.ProfileServiceFactory;
 import org.jetbrains.annotations.NotNull;
 
 import org.jboss.deployers.spi.management.ManagementView;
+import org.jboss.deployers.spi.management.NameMatcher;
 import org.jboss.managed.api.ComponentType;
 import org.jboss.managed.api.ManagedComponent;
 import org.jboss.managed.api.ManagedProperty;
 import org.jboss.managed.api.annotation.ViewUse;
-import org.jboss.metatype.api.values.SimpleValue;
-import org.jboss.metatype.api.values.EnumValue;
 import org.jboss.metatype.api.types.MetaType;
+import org.jboss.metatype.api.values.EnumValue;
+import org.jboss.metatype.api.values.SimpleValue;
 
 /**
+ * A collection of utility methods for working with Profile Service {@link ManagedComponent}s.
+ *
  * @author Ian Springer
  */
 public class ManagedComponentUtils
 {
-    public static ManagedComponent getManagedComponent(ComponentType componentType, String componentName)
+    private static final Comparator<ComponentType> COMPONENT_TYPE_COMPARATOR = new ComponentTypeComparator();
+
+    public static ManagedComponent getManagedComponent(ManagementView managementView, ComponentType componentType,
+                                                       String componentName)
     {
-        Set<ManagedComponent> components = getManagedComponents(componentType);
+        Set<ManagedComponent> components = getManagedComponents(managementView, componentType);
         for (ManagedComponent component : components)
         {
             if (component.getName().equals(componentName))
@@ -50,9 +61,10 @@ public class ManagedComponentUtils
         return null;
     }
 
-    public static ManagedComponent getSingletonManagedComponent(ComponentType componentType)
+    public static ManagedComponent getSingletonManagedComponent(ManagementView managementView,
+                                                                ComponentType componentType)
     {
-        Set<ManagedComponent> components = getManagedComponents(componentType);
+        Set<ManagedComponent> components = getManagedComponents(managementView, componentType);
         if (components.size() != 1)
             throw new IllegalStateException("Found more than one component of type " + componentType + ": "
                     + components);
@@ -64,15 +76,23 @@ public class ManagedComponentUtils
     public static Serializable getSimplePropertyValue(ManagedComponent component, String propertyName)
     {
         ManagedProperty property = component.getProperty(propertyName);
+        if (property == null)
+            throw new IllegalStateException("Property named '" + propertyName + "' not found for ManagedComponent ["
+                    + component + "].");
         MetaType metaType = property.getMetaType();
         Serializable value;
-        if (metaType.isSimple()) {
+        if (metaType.isSimple())
+        {
             SimpleValue simpleValue = (SimpleValue)property.getValue();
             value = (simpleValue != null) ? simpleValue.getValue() : null;
-        } else if (metaType.isEnum()) {
+        }
+        else if (metaType.isEnum())
+        {
             EnumValue enumValue = (EnumValue)property.getValue();
             value = (enumValue != null) ? enumValue.getValue() : null;
-        } else {
+        }
+        else
+        {
             throw new IllegalStateException("Type of [" + property + "] is not simple or enum.");
         }
         return value;
@@ -82,17 +102,59 @@ public class ManagedComponentUtils
     public static EnumSet<ViewUse> getViewUses(ManagedProperty managedProperty)
     {
         EnumSet<ViewUse> viewUses = EnumSet.noneOf(ViewUse.class);
-        for (ViewUse viewUse : ViewUse.values()) {
-           if (managedProperty.hasViewUse(viewUse))
-               viewUses.add(viewUse);
+        for (ViewUse viewUse : ViewUse.values())
+        {
+            if (managedProperty.hasViewUse(viewUse))
+                viewUses.add(viewUse);
         }
         return viewUses;
     }
 
-    @NotNull
-    private static Set<ManagedComponent> getManagedComponents(ComponentType componentType)
+    /**
+     * TODO
+     */
+    public static boolean isManagedComponent(ManagementView managementView, String name, ComponentType componentType)
     {
-        ManagementView managementView = ProfileServiceFactory.getCurrentProfileView();
+        boolean isDeployed = false;
+        if (name != null)
+        {
+            try
+            {
+                ManagedComponent component = getManagedComponent(managementView, componentType, name);
+                if (component != null)
+                    isDeployed = true;
+            }
+            catch (Exception e)
+            {
+                // Setting it to true to be safe than sorry, since there might be a component
+                // already deployed in the AS. TODO (ips): I don't think I like this.
+                isDeployed = true;
+            }
+        }
+        return isDeployed;
+    }
+
+    @NotNull
+    public static Set<ManagedComponent> getManagedComponents(ManagementView managementView, ComponentType componentType,
+                                                             String name, NameMatcher<ManagedComponent> nameMatcher)
+    {
+        Set<ManagedComponent> matchingComponents = new HashSet<ManagedComponent>();
+        Set<ManagedComponent> allComponents = getManagedComponents(managementView, componentType);
+        for (ManagedComponent component : allComponents) {
+            if (nameMatcher.matches(component, name)) {
+                matchingComponents.add(component);
+            }
+        }
+        return matchingComponents;
+    }
+
+    public static Comparator<ComponentType> getComponentTypeComparator() {
+        return COMPONENT_TYPE_COMPARATOR;
+    }
+
+    @NotNull
+    private static Set<ManagedComponent> getManagedComponents(ManagementView managementView, ComponentType componentType)
+    {
         Set<ManagedComponent> components;
         try
         {
@@ -103,5 +165,15 @@ public class ManagedComponentUtils
             throw new IllegalStateException(e);
         }
         return components;
+    }
+
+    private static class ComponentTypeComparator implements Comparator<ComponentType>
+    {
+        public int compare(ComponentType type1, ComponentType type2)
+        {
+            int value = type1.getType().compareTo(type2.getType());
+            // If the categories (e.g. JMSDestination) were equal, do a secondary sort by subtype (e.g. Queue).
+            return (value != 0) ? value : type1.getSubtype().compareTo(type2.getSubtype());
+        }
     }
 }
