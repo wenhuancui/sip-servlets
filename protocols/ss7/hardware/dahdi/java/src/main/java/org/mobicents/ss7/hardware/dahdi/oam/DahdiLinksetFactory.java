f6ece4e5d8f9f8d17ee7f71f6834e5ae4ee1137f
pavel.slegr <pavel.slegr@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2011-01-14 18:11:59 +0000

    jbcp 5.x update - SS7
    git-svn-id: https://mobicents.googlecode.com/svn/branches/jbcp/5.x-mead@16429 bf0df8d0-2c1f-0410-b170-bd30377b63dc

---
+++ protocols/ss7/hardware/dahdi/java/src/main/java/org/mobicents/ss7/hardware/dahdi/oam/DahdiLinksetFactory.java
@@ -0,0 +1,66 @@
+package org.mobicents.ss7.hardware.dahdi.oam;
+
+import org.mobicents.ss7.linkset.oam.LinkOAMMessages;
+import org.mobicents.ss7.linkset.oam.Linkset;
+import org.mobicents.ss7.linkset.oam.LinksetFactory;
+
+/**
+ * <p>
+ * Factory for creating <tt>dahdi</tt> based {@link Linkset}
+ * </p>
+ * 
+ * @author amit bhayani
+ * 
+ */
+public class DahdiLinksetFactory extends LinksetFactory {
+
+    private static final String NAME = "dahdi";
+
+    public DahdiLinksetFactory() {
+    }
+
+    @Override
+    public String getName() {
+        return NAME;
+    }
+
+    @Override
+    public Linkset createLinkset(String[] options) throws Exception {
+
+        // the expected command is "linkset create dahdi opc 1 apc 2 ni 3
+        // linkset1". We know length is 10
+        if (options.length != 10) {
+            throw new Exception(LinkOAMMessages.INVALID_COMMAND);
+        }
+
+        String option = options[3];
+
+        // If first option is not OPC
+        if (option.compareTo("opc") != 0) {
+            return null;
+        }
+
+        int opc = Integer.parseInt(options[4]);
+
+        if (options[5].compareTo("apc") != 0) {
+            throw new Exception(LinkOAMMessages.INVALID_COMMAND);
+        }
+
+        int dpc = Integer.parseInt(options[6]);
+
+        if (options[7].compareTo("ni") != 0) {
+            throw new Exception(LinkOAMMessages.INVALID_COMMAND);
+        }
+
+        int ni = Integer.parseInt(options[8]);
+
+        String name = options[9];
+
+        if (name == null) {
+            throw new Exception(LinkOAMMessages.INVALID_COMMAND);
+        }
+
+        return new DahdiLinkset(name, opc, dpc, ni);
+    }
+
+}
