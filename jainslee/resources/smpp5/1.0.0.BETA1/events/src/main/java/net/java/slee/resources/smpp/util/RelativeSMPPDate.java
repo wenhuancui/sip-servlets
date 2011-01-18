ad3b6e4d55a1cbd842c2321f4c9c39054e0a327a
pavel.slegr <pavel.slegr@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2011-01-18 15:36:47 +0000

    git-svn-id: https://mobicents.googlecode.com/svn/branches/jbcp/5.x-mead@16623 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- jainslee/resources/smpp5/1.0.0.BETA1/events/src/main/java/net/java/slee/resources/smpp/util/RelativeSMPPDate.java
+++
@@ -1,5 +0,0 @@
-package net.java.slee.resources.smpp.util;
-
-public interface RelativeSMPPDate extends SMPPDate {
-
-}
--- jainslee/resources/smpp5/1.0.0.BETA1/ra/src/main/java/org/mobicents/slee/resources/smpp/ExtSmppResponse.java
+++
@@ -1,8 +0,0 @@
-package org.mobicents.slee.resources.smpp;
-
-import net.java.slee.resources.smpp.pdu.SmppResponse;
-
-public interface ExtSmppResponse extends SmppResponse {
-
-	org.mobicents.protocols.smpp.message.SMPPPacket getSMPPPacket();
-}
