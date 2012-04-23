17f9e38b2acdbb312dce3f0cff324904ca9b1af4
Jean Deruelle <jean.deruelle@gmail.com>
2012-04-23 18:27:04 +0200

    moving back to a lower value as the tests are running on a more performing instance now

--- sip-servlets-test-suite/sipp-scenarios/clustering/auto-clustering-failover-test.sh
+++ sip-servlets-test-suite/sipp-scenarios/clustering/auto-clustering-failover-test.sh
@@ -3,8 +3,8 @@ export EXAMPLES_HOME=../../../sip-servlets-examples
 export config1="all"
 export config2="port-1"
 export KILL_PARAMS="-9"
-export FULLSTARTSLEEP=700
-export HALFSTARTSLEEP=700
+export FULLSTARTSLEEP=200
+export HALFSTARTSLEEP=200
 export CALLS=5
 
 if [ "x$1" != "x" ]; then
